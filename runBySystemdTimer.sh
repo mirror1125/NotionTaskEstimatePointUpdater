5f753b1766d67000af4212816d6d9cf9005b2be3
mirrorman <24805432+mirror1125@users.noreply.github.com>
2025-03-21 16:44:49 +0900

    add runBySystemdTimer.sh

---
+++ runBySystemdTimer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/mirrorman/workspace/NotionTaskEstimatePointUpdater/.venv/bin/activate
+exec python /home/mirrorman/workspace/NotionTaskEstimatePointUpdater/notionTaskEstimatePointUpdater.py
